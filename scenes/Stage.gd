b8e597613e81c2d2b9058e03555e5e0f1e6bd7b8
Belén Albeza <belen.albeza@gmail.com>
2019-11-27 20:39:31 +0100

    implement player score

--- scenes/Stage.gd
+++ scenes/Stage.gd
@@ -4,12 +4,15 @@ export (int) var SCROLL_SPEED = 100
 export (PackedScene) var ObstacleScene = null
 export (int) var SPAWN_PROBABILITY = 85
 export (int) var SPAWN_OFFSET_X = 100
+export (float) var SCORE_DAMPING = 0.05
 
 # Declare member variables here. Examples:
 var level_x = 0
 var is_game_over = false
 var ground_y = 0
 var screen_width = 0
+
+var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,6 +24,7 @@ func _process(delta):
 	if not is_game_over:
 		level_x += SCROLL_SPEED * delta
 		$Character.position.x = level_x
+		self._update_score(floor(level_x * SCORE_DAMPING))
 
 func _on_Character_died():
 	get_tree().reload_current_scene()
@@ -37,3 +41,7 @@ func _spawn_obstacle():
 	obstacle.position.y = ground_y
 	obstacle.position.x = level_x + screen_width + rand_range(0, SPAWN_OFFSET_X)
 	$Obstacles.add_child(obstacle)
+
+func _update_score(value):
+	score = value
+	$UILayer/ScoreLabel.text = "Score: " + str(value)
